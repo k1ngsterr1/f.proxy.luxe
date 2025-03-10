f9928b5a6e3f6d665042ebb7d573a09b4627f6f0
Ruslan Makhmatov <103198805+k1ngsterr1@users.noreply.github.com>
2025-03-10 23:11:59 +0500

    fix

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,65 +1,64 @@
-import {FC} from "react";
-import Image from "next/image";
 import Logo from "@/assets/images/logo.png";
 import TelegramIcon from "@/assets/images/vk-icon.png";
-import Payment from "@/assets/images/payment.png";
+import Image from "next/image";
 import Link from "next/link";
+import { FC } from "react";
 
 export const Footer: FC = () => {
-    return (
-        <>
-            <div className="payment">
-                <Image src={Payment} alt="Payment" />
+  return (
+    <>
+      <footer className="footer">
+        <div className="container">
+          <div className="footer-inner">
+            <div className="footer-logo">
+              <Link href="/" className="header-logo">
+                <Image src={Logo} alt="Proxy Luxe" />
+              </Link>
+              <p className="footer-logo__text">
+                Индивидуальные резидентные прокси
+              </p>
+              <p className="footer-logo__text">
+                © 2021 – {new Date().getFullYear()} «Proxy.Luxe»
+              </p>
             </div>
-            <footer className="footer">
-                <div className="container">
-                    <div className="footer-inner">
-                        <div className="footer-logo">
-                            <Link href="/" className="header-logo">
-                                <Image
-                                    src={Logo}
-                                    alt="Proxy Luxe"
-                                />
-                            </Link>
-                            <p className="footer-logo__text">Индивидуальные резидентные прокси</p>
-                            <p className="footer-logo__text">© 2021 – {new Date().getFullYear()} «Proxy.Luxe»</p>
-                        </div>
-                        <div className="footer-menu">
-                            <nav className="footer-menu__nav">
-                                <a href="#">Купить прокси</a>
-                                <a href="#">Цены</a>
-                                <a href="#">FAQ</a>
-                                <a href="#">Блог</a>
-                                <a href="#">Мой IP</a>
-                                <a href="#">Прокси чекер</a>
-                                <a href="#">API</a>
-                                <a href="#">Условия использования</a>
-                                <a href="#">Реквизиты</a>
-                            </nav>
-                            <div className="footer-menu__info">
-                                <a href="#">Политика конфиденциальности</a>
-                                <div className="separator"></div>
-                                <a href="#">Уведомление об ответственности</a>
-                            </div>
-                        </div>
-                        <div className="footer-soc">
-                            <p className="footer-soc__hint">Техническая поддержка:</p>
-                            <a href="mailto:admin@proxy.luxe" className="footer-soc__link">admin@proxy.luxe</a>
-                            <nav className="footer-soc__links">
-                                <a href="#">
-                                    <Image
-                                        src={TelegramIcon}
-                                        alt="Telegram"
-                                        layout="response"
-                                        width={TelegramIcon.width}
-                                        height={TelegramIcon.height}
-                                    />
-                                </a>
-                            </nav>
-                        </div>
-                    </div>
-                </div>
-            </footer>
-        </>
-    )
-}
+            <div className="footer-menu">
+              <nav className="footer-menu__nav">
+                <a href="#">Купить прокси</a>
+                <a href="#">Цены</a>
+                <a href="#">FAQ</a>
+                <a href="#">Блог</a>
+                <a href="#">Мой IP</a>
+                <a href="#">Прокси чекер</a>
+                <a href="#">API</a>
+                <a href="#">Условия использования</a>
+                <a href="#">Реквизиты</a>
+              </nav>
+              <div className="footer-menu__info">
+                <a href="#">Политика конфиденциальности</a>
+                <div className="separator"></div>
+                <a href="#">Уведомление об ответственности</a>
+              </div>
+            </div>
+            <div className="footer-soc">
+              <p className="footer-soc__hint">Техническая поддержка:</p>
+              <a href="mailto:admin@proxy.luxe" className="footer-soc__link">
+                admin@proxy.luxe
+              </a>
+              <nav className="footer-soc__links">
+                <a href="#">
+                  <Image
+                    src={TelegramIcon}
+                    alt="Telegram"
+                    layout="response"
+                    width={TelegramIcon.width}
+                    height={TelegramIcon.height}
+                  />
+                </a>
+              </nav>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+};
--- components/forms/register.form.tsx
+++ components/forms/register.form.tsx
@@ -1,15 +1,19 @@
 export const RegisterForm = () => {
-    return (
-        <form action="#">
-            <input type="text" className="auth-inp auth-mail" placeholder="E-mail"/>
-            <input type="text" className="auth-inp auth-pass" placeholder="Пароль"/>
-            <input type="text" className="auth-inp auth-pass" placeholder="Повторить пароль"/>
-            <div className="btn-wrap">
-                <button className="auth-btn btn">регистрация</button>
-            </div>
-            {/*<div className="btn-wrap">*/}
-            {/*    <a href="#" className="auth-link">Войти</a>*/}
-            {/*</div>*/}
-        </form>
-    )
-}
+  return (
+    <form action="#">
+      <input type="text" className="auth-inp auth-mail" placeholder="E-mail" />
+      <input type="text" className="auth-inp auth-pass" placeholder="Пароль" />
+      <input
+        type="text"
+        className="auth-inp auth-pass"
+        placeholder="Повторить пароль"
+      />
+      <div className="btn-wrap">
+        <button className="auth-btn btn">регистрация</button>
+      </div>
+      {/*<div className="btn-wrap">*/}
+      {/*    <a href="#" className="auth-link">Войти</a>*/}
+      {/*</div>*/}
+    </form>
+  );
+};
