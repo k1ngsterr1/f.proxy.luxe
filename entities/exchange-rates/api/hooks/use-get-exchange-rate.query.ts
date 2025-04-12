f0c0b1f262380df943b2241649f65b5a086b830a
Erlan-Bam <erlanzh.gg@gmail.com>
2025-04-12 19:41:39 +0500

    feat: updated

--- entities/exchange-rates/api/hooks/use-get-exchange-rate.query.ts
+++ entities/exchange-rates/api/hooks/use-get-exchange-rate.query.ts
@@ -1,10 +1,15 @@
 import { useQuery } from "@tanstack/react-query";
 import { getExchangeRate } from "../get/get-exchange-rate.api";
-
-export const useGetExchangeRate = () => {
+type Params = {
+  amount: number;
+  from: string;
+  to: string;
+};
+export const useGetExchangeRate = ({ amount, from, to }: Params) => {
   return useQuery<number | null, Error>({
-    queryKey: ["exchangeRate"],
-    queryFn: getExchangeRate,
+    queryKey: ["exchangeRate", amount, from, to],
+    queryFn: () => getExchangeRate(amount, from, to),
+    enabled: !!amount && !!from && !!to,
     staleTime: 1000 * 60 * 10, // ✅ Cache data for 10 minutes
     retry: 2, // ✅ Retry twice if it fails
     refetchOnWindowFocus: false, // ✅ Prevent unnecessary refetches
