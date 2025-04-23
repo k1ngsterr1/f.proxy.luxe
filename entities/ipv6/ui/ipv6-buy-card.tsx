6258ed1e1bd5781e399da53f22b006a5eb2a18f3
k1ngsterr1 <ruslanmakhmatov@gmail.com>
2025-04-23 18:08:26 +0500

    fix

--- entities/ipv6/ui/ipv6-buy-card.tsx
+++ entities/ipv6/ui/ipv6-buy-card.tsx
@@ -183,7 +183,6 @@ export const IPV6BuyCard = () => {
                 : "1px solid #3E3E3E",
             }}
           >
-            {/* Added placeholder option with empty value */}
             <option value="">{i18n("goals.placeholder")}</option>
             <option value="surfing">{i18n("goals.surfing")}</option>
             <option value="socialMedia">{i18n("goals.socialMedia")}</option>
