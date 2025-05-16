c4b68b6aeb06350f6384e3593c148ef3e3f0c10e
k1ngsterr1 <ruslanmakhmatov@gmail.com>
2025-05-16 19:34:03 +0500

    fix

--- entities/residental-proxy/ui/residental-proxy-buy-card.tsx
+++ entities/residental-proxy/ui/residental-proxy-buy-card.tsx
@@ -151,7 +151,10 @@ export const ResidentalProxyBuyCard = () => {
         </h4>
         <div style={selectWrapperStyle}>
           <select className="buy-item__select" style={selectStyle}>
-            <option value="∞">{i18n("option")}</option>
+            <option value="∞">
+              Создавайте любое кол-во прокси после оплаты тарифа в личном
+              кабинете.
+            </option>
           </select>
           <ChevronDown style={chevronStyle} size={16} />
         </div>
@@ -165,7 +168,10 @@ export const ResidentalProxyBuyCard = () => {
         </h4>
         <div style={selectWrapperStyle}>
           <select className="buy-item__select" style={selectStyle}>
-            <option value="∞">∞</option>
+            <option value="∞">
+              Создавайте любое кол-во прокси после оплаты тарифа в личном
+              кабинете.
+            </option>
           </select>
           <ChevronDown style={chevronStyle} size={16} />
         </div>
